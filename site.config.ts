786ea5a86bb47cba83cdec5d45507aae763794fc
kangdonghyuck07 <mrkangkdh@gmail.com>
2026-05-21 10:32:27 +0900

    내 노션페이지 id로 수정

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '317661362ab2816ab875f2c5f69dc488',
+  rootNotionPageId: 'bb18c99c53cf834b8bc2014b52b6f7f0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
